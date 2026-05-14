1e61b23fa5f2dbb84f163553dd745d8d77bd2f53
Dedes <federico.lepore73@gmail.com>
2026-05-14 20:57:43 +0200

    new repo

---
+++ src/test/java/federicolepore/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package federicolepore.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
